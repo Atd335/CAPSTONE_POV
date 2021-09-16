36f00f2a1d82274b4494c33b803ba5b526a9200c
Andrew <atd335@nyu.edu>
2021-09-16 13:17:55 -0400

    v0_4
    Primary Tuning of 2D controls

--- POV standard 3D experimentation/Assets/Scripts/ImageCap.cs
+++ POV standard 3D experimentation/Assets/Scripts/ImageCap.cs
@@ -8,6 +8,11 @@ using UnityEngine.Rendering;
 public class ImageCap : MonoBehaviour
 {
 
+    //Debug Stuff
+    bool startSim;
+    int whileChecker;
+    int collisionTimeOut = 100;
+
     //Cameras
     Camera CollisionCamera;
     Camera VisualCamera;
@@ -17,25 +22,33 @@ public class ImageCap : MonoBehaviour
     public Texture2D texture;
 
     //Simple Character
-
     List<Vector3> collisionVectors;
 
     public Transform player;
     float playerRadius = 25;
     float playerRadiusScaled;
     Vector3 moveDirection;
-    public bool grounded;
-
+    bool grounded;
+    bool groundedForJump;
+    bool roofed;
+    float inputDirection;
+    bool risingJump;
+    float gravityMultiplier = 1;
 
     //screen units for movement
     public float jumpHeight;
-    public float playerSpd = 20;
-    public float grav = 20;
-    float playerSpdScaled;
+    public float playerSpd;
+    public float grav;
+    public float acceleration;
+    public float decelleration;
+    public float maxSpd;
+
     float playerGravScaled;
     float scaledPixelSize;
     float scaledJumpHeight;
-
+    float scaledAccel;
+    float scaledDeccel;
+    float scaledMaxSpd;
     
 
     private void Start()
@@ -50,10 +63,15 @@ public class ImageCap : MonoBehaviour
         {
             VisualCamera.enabled = !VisualCamera.enabled;
         }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            startSim = true;
+        }
     }
 
     private void LateUpdate()
     {
+        if (!startSim) { return; }
         if (!texture) { return; }
         updateRelativeUnits();
         movePlayer();
@@ -64,43 +82,95 @@ public class ImageCap : MonoBehaviour
         scaledPixelSize = Screen.width / 1920f;
 
         playerRadiusScaled = playerRadius * scaledPixelSize;
-        playerSpdScaled = playerSpd * scaledPixelSize;
         playerGravScaled = grav*scaledPixelSize;
         scaledJumpHeight = jumpHeight * scaledPixelSize;
+        scaledAccel = acceleration * scaledPixelSize;
+        scaledDeccel = decelleration * scaledPixelSize;
+        scaledMaxSpd = maxSpd * scaledPixelSize;
     }
 
     void movePlayer()
     {
 
-        moveDirection = new Vector3(Input.GetAxis("Horizontal"), moveDirection.y, 0);
-        grounded = isGrounded();
-
-        if (grounded)
+        moveDirection = new Vector3(moveDirection.x, moveDirection.y, 0);
+        
+        if (Input.GetAxisRaw("Horizontal") != 0)
         {
-            moveDirection.y = -.01f;
-            if (Input.GetKeyDown(KeyCode.Space)){ moveDirection.y = jumpHeight; };
+            playerSpd += scaledAccel * Time.deltaTime;
+            inputDirection = Input.GetAxisRaw("Horizontal");
         }
         else
         {
-            moveDirection.y -= playerGravScaled * Time.deltaTime;
+            playerSpd -= scaledDeccel * Time.deltaTime;
         }
 
-        player.position += moveDirection * playerSpdScaled * Time.deltaTime;
+        playerSpd = Mathf.Clamp(playerSpd, 0, scaledMaxSpd);
 
+        groundedForJump = isGroundedForjump();
+        grounded = isGrounded();
+        roofed = isRoofed();
+
+        if (grounded)
+        {
+            gravityMultiplier = 1;
+            moveDirection.y = -.05f;
+            
+        }
+        else
+        {
+            moveDirection.y -= playerGravScaled * Time.deltaTime * gravityMultiplier;
+            if (moveDirection.y > 0)
+            {
+                if (Input.GetButtonUp("Jump"))
+                {
+                    gravityMultiplier = 2.75f;
+                }
+                risingJump = true;
+            }
+            if (risingJump && moveDirection.y <= 0)
+            {
+                risingJump = false;
+            }
+        }
+
+        if (Input.GetButtonDown("Jump") && groundedForJump) { moveDirection.y = scaledJumpHeight; };
+
+        if (roofed && moveDirection.y > 0) { moveDirection.y = 0; }
+
+        moveDirection.x = playerSpd * inputDirection;
+        player.position += moveDirection  * Time.deltaTime;
+
+        whileChecker = 0;
         while (isOverlappingBlack(player.position, playerRadius))
         {
+            whileChecker++;
             foreach (Vector3 cv in collisionVectors)
             {
                 player.position -= cv * scaledPixelSize;
             }
+            if (whileChecker > collisionTimeOut) { Destroy(player.gameObject); }
         }
     }
 
+    bool isGroundedForjump()
+    {
+        Vector3Int v = roundVectorToInt(player.position);
+        Color[] groundPixels = texture.GetPixels(v.x - (Mathf.RoundToInt(playerRadiusScaled) / 2), v.y - (Mathf.RoundToInt(playerRadiusScaled) + (Mathf.RoundToInt(8 * scaledPixelSize))), Mathf.RoundToInt(playerRadiusScaled) / 2, 1);
+
+        return groundPixels.Contains<Color>(Color.black);
+    }
     bool isGrounded()
     {
         Vector3Int v = roundVectorToInt(player.position);
-        Color[] groundPixels = texture.GetPixels(v.x-(Mathf.RoundToInt(playerRadiusScaled)), v.y - (Mathf.RoundToInt(playerRadiusScaled)+3), 50, 1);
+        Color[] groundPixels = texture.GetPixels(v.x-(Mathf.RoundToInt(playerRadiusScaled)/2), v.y - (Mathf.RoundToInt(playerRadiusScaled) + (Mathf.RoundToInt(3*scaledPixelSize))), Mathf.RoundToInt(playerRadiusScaled)/2, 1);
         
+        return groundPixels.Contains<Color>(Color.black);
+    }
+    bool isRoofed()
+    {
+        Vector3Int v = roundVectorToInt(player.position);
+        Color[] groundPixels = texture.GetPixels(v.x - (Mathf.RoundToInt(playerRadiusScaled)), v.y + (Mathf.RoundToInt(playerRadiusScaled) + (Mathf.RoundToInt(3 * scaledPixelSize))), Mathf.RoundToInt(playerRadiusScaled), 1);
+
         return groundPixels.Contains<Color>(Color.black);
     }
 
