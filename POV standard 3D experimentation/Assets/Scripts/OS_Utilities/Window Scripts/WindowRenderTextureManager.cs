506d3635ef979fa98f5163c2f7bc4d289407fa3f
Andrew <atd335@nyu.edu>
2021-11-09 11:34:03 -0500

    Reorganized Project Files

---
+++ POV standard 3D experimentation/Assets/Scripts/OS_Utilities/Window Scripts/WindowRenderTextureManager.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WindowRenderTextureManager : MonoBehaviour
+{
+
+    public Window_Resizer WR;
+    public Window_Content_Manager WCM;
+    RenderTexture RT;
+
+
+    void Awake()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RT = new RenderTexture(
+            Mathf.RoundToInt(WR.contentSection.rectTransform.sizeDelta.x),
+            Mathf.RoundToInt(WR.contentSection.rectTransform.sizeDelta.y),
+            16, RenderTextureFormat.ARGB32);
+
+        RT.Create();
+
+        WCM.viewCam.targetTexture = RT;
+
+        WR.contentSectionRT.texture = RT;
+    }
+}
