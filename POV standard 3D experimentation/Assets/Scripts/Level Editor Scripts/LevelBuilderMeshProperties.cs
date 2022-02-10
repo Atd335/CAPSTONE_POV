489d1043cf2275e74c8ec3c532b3cbc6eb4fc7b9
Andrew <atd335@nyu.edu>
2022-02-10 14:09:07 -0500

    Blender Mesh

--- POV standard 3D experimentation/Assets/Scripts/Level Editor Scripts/LevelBuilderMeshProperties.cs
+++ POV standard 3D experimentation/Assets/Scripts/Level Editor Scripts/LevelBuilderMeshProperties.cs
@@ -47,16 +47,16 @@ public class LevelBuilderMeshProperties : MonoBehaviour
 
         ////CSG is so strange and finicky, before a build attempt, comment out the foreach loop below. this is an editor only tool anyways. 
 
-        //foreach (RealtimeCSG.Components.CSGBrush b in GetComponentsInChildren<RealtimeCSG.Components.CSGBrush>())
-        //{
-        //    for (int i = 0; i < b.Shape.TexGens.Length; i++)
-        //    {
-        //        Material m = new Material(Shader.Find("Standard"));
-        //        m.color = modelColor;
-        //        b.Shape.TexGens[i].RenderMaterial = m;
-        //        b.transform.position += Vector3.up * .0001f;
-        //    }
-        //}
+        foreach (RealtimeCSG.Components.CSGBrush b in GetComponentsInChildren<RealtimeCSG.Components.CSGBrush>())
+        {
+            for (int i = 0; i < b.Shape.TexGens.Length; i++)
+            {
+                Material m = new Material(Shader.Find("Standard"));
+                m.color = modelColor;
+                b.Shape.TexGens[i].RenderMaterial = m;
+                b.transform.position += Vector3.up * .0001f;
+            }
+        }
     }
 
     void Awake()
