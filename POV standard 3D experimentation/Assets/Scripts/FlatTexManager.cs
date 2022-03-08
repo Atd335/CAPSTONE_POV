aca929e0acc70f706ca1be781dce42ed2b2f2c67
Andrew <atd335@nyu.edu>
2022-03-07 20:02:04 -0500

    Some Changes to Level for Midterm

--- POV standard 3D experimentation/Assets/Scripts/FlatTexManager.cs
+++ POV standard 3D experimentation/Assets/Scripts/FlatTexManager.cs
@@ -9,14 +9,18 @@ public class FlatTexManager : MonoBehaviour
     float lowPacity = .3f;
     float highPacity = 1f;
 
+    public Image white;
+
     void Update()
     {
         if (UpdateController.switcher.fpsMode)
         {
+            white.color = Color.Lerp(white.color, new Color(1,1,1,0), Time.deltaTime * 10);
             GetComponent<RawImage>().color = Color.Lerp(GetComponent<RawImage>().color, new Color(1,1,1,lowPacity), Time.deltaTime * 10);
         }
         else
         {
+            white.color = Color.Lerp(white.color, new Color(1, 1, 1, .9f), Time.deltaTime * 10);
             GetComponent<RawImage>().color = Color.Lerp(GetComponent<RawImage>().color, new Color(1, 1, 1, highPacity), Time.deltaTime * 10);
         }
     }
