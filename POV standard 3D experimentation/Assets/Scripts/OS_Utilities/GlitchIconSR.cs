4a66fbf508d7bf8401b0e058ae9fd12d7cbb9590
Andrew <atd335@nyu.edu>
2022-04-18 10:50:53 -0400

    Added glitch effects for Sprite renderers and Particle Materials
    Also made an example in the tester scene

--- POV standard 3D experimentation/Assets/Scripts/OS_Utilities/GlitchIconSR.cs
+++ POV standard 3D experimentation/Assets/Scripts/OS_Utilities/GlitchIconSR.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine.UI;
 using UnityEngine;
 
-public class GlitchIcon : MonoBehaviour
+public class GlitchIconSR : MonoBehaviour
 {
     int fr;
     public Sprite[] sprites;
@@ -12,7 +12,7 @@ public class GlitchIcon : MonoBehaviour
         fr++;
         if (fr % Random.Range(1, 10)==0)
         {
-            GetComponent<Image>().sprite = sprites[Random.Range(0,sprites.Length)]; 
+            GetComponent<SpriteRenderer>().sprite = sprites[Random.Range(0,sprites.Length)];
         }
     }
 
